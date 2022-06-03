b9e3d6844608d67589672ccdbb61c6dcc721d2fd
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-06-03 23:04:21 +0200

    Got done with Symfony, so I ditched it.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
-
-use App\Kernel;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+require_once __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/../.env');
+$dotenv->load();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/../.env.marapi-local');
+$dotenv->safeLoad();
+echo "Mar's api - hosted by <font color=\"orange\">" . $_ENV['hostedby'] . "</font> written by <a href=\"https://github.com/mar-on-github/\">mar</a>.";
+?>
+<h1>I think you may have stumbled upon something that is not meant for humans to stumble upon!</h1>
+<p><i>Other places that <u>ARE</u> meant for humans to stumble upon:</i></p>
+<ul>
+<li><a href="https://blog.from-mar.com">My blog</a></li>
+</ul>
+<p><b>Stay safe on your travels, dear friend!</b></p>
