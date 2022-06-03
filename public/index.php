c71d3acc10799c56c9dbe5698f96190848a94a1d
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-06-03 23:58:27 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,23 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/../.env');
 $dotenv->load();
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '/../.env.marapi-local');
 $dotenv->safeLoad();
-echo "Mar's api - hosted by <font color=\"orange\">" . $_ENV['hostedby'] . "</font> written by <a href=\"https://github.com/mar-on-github/\">mar</a>.";
 ?>
-<h1>I think you may have stumbled upon something that is not meant for humans to stumble upon!</h1>
-<p><i>Other places that <u>ARE</u> meant for humans to stumble upon:</i></p>
-<ul>
-<li><a href="https://blog.from-mar.com">My blog</a></li>
-</ul>
-<p><b>Stay safe on your travels, dear friend!</b></p>
+<html>
+    <title>Woops!</title>
+
+    <body>
+        <?php
+        echo "<p>Mar's api - hosted by <font color=\"orange\">" . $_ENV['hostedby'] . "</font> written by <a href=\"https://github.com/mar-on-github/\">mar</a>.</p>";
+        ?>
+        <h1>I think you may have stumbled upon something that is not meant for humans to stumble upon!</h1>
+        <p><i>Other places that <u>ARE</u> meant for humans to stumble upon:</i></p>
+        <ul>
+            <li><a href="https://blog.from-mar.com">My blog</a></li>
+        </ul>
+        <p><b>Stay safe on your travels, dear friend!</b></p>
+    </body>
+
+</html>
