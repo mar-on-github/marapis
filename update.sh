2cab297471e7ac0249d466f7d9ab571a445221ae
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-06-04 15:11:46 +0200

    add local update file functionality

--- update.sh
+++ update.sh
@@ -1,1 +1,12 @@
+echo "Running 'git pull' from root of marapis clone."
 git pull
+echo "------------------------------------------------"
+echo "Checking for local update scripts..."
+if [ -f "./update.local.sh" ]; then
+    echo "./update.local.sh exists. Will now be called."
+    bash ./update.local.sh
+else 
+    echo "./update.local.sh does not exist. Will not be called."
+fi
+echo "------------------------------------------------"
+echo "Update script executed." 
