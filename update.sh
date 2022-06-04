c76200ee5d80220c61bcfd540f9215980e0277e4
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-06-04 15:24:41 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ git pull
 echo "------------------------------------------------"
 echo "Checking for local update scripts..."
 if [ -f "../update.local.sh" ]; then
-    echo "../update.local.sh exists. Will now be called. <font style=\"background-color: #CC9999\">"
+    echo "<font style=\"color: green\">../update.local.sh exists. Will now be called.</font><font style=\"background-color: #CC9999\">"
     bash ../update.local.sh
     echo "</font>"
 else 
-    echo "../update.local.sh does not exist. Will not be called."
+    echo "<font style=\"color: yellow\">../update.local.sh does not exist. Will not be called.</font>"
 fi
 echo "------------------------------------------------"
 echo "Update script executed. </pre><br><h3>Current directory:</h3><pre style=\"border: 5px solid; background-color: #33CCCC\">"
